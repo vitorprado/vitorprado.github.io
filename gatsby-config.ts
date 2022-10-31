f4e2bd9af8307a750b4788139bd0b43b7226f7dc
Vitor Prado <jvirtuprado@gmail.com>
2022-10-31 00:29:39 -0300

    Add gh pages config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -6,49 +6,49 @@ const config: GatsbyConfig = {
 		title: `Vitor Prado`,
 		siteUrl: `https://vitorprado.dev`
 	},
-		graphqlTypegen: true,
-		plugins: [
-			{
-				resolve: 'gatsby-plugin-google-analytics',
-				options: {
-					"trackingId": "UA-54273286-1"
-				}
-			}, 
-			"gatsby-plugin-image", 
-			{
-				resolve: 'gatsby-plugin-manifest',
-				options: {
-					"icon": "src/images/icon.png"
-				}
-			}, 
-			"gatsby-transformer-remark", 
-			"gatsby-plugin-sharp", 
-			"gatsby-transformer-sharp", 
-			{
-				resolve: 'gatsby-source-filesystem',
-				options: {
-					"name": "images",
-					"path": "./src/images/"
-				},
-				__key: "images"
-			}, 
-			{
-				resolve: 'gatsby-source-filesystem',
-				options: {
-					"name": "pages",
-					"path": "./src/pages/"
-				},
-				__key: "pages"
+	graphqlTypegen: true,
+	plugins: [
+		{
+			resolve: 'gatsby-plugin-google-analytics',
+			options: {
+				"trackingId": "UA-54273286-1"
+			}
+		},
+		"gatsby-plugin-image",
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				"icon": "src/images/icon.png"
+			}
+		},
+		"gatsby-transformer-remark",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				"name": "images",
+				"path": "./src/images/"
 			},
-			{
-				resolve: `gatsby-theme-jsonresume`,
-				options: {
-					name: "resume",
-					theme: "jsonresume-theme-vp",
-					resumeJson: Resume
-				},
+			__key: "images"
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				"name": "pages",
+				"path": "./src/pages/"
 			},
-		]
+			__key: "pages"
+		},
+		{
+			resolve: `gatsby-theme-jsonresume`,
+			options: {
+				name: "resume",
+				theme: "jsonresume-theme-vp",
+				resumeJson: Resume
+			},
+		},
+	]
 };
 
 export default config;
